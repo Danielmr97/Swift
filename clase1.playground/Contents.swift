fc655a263a364fbd864750e1cdd8eea02feb1155
Danielmr97 <jesusdmr97@gmail.com>
2020-02-11 16:12:02 -0600

    Add files via upload

---
+++ clase1.playground/Contents.swift
@@ -0,0 +1,18 @@
+
+
+
+import UIKit
+/*
+ String
+ Enteros "Int"
+ Numeros com punto decimal "Double o Float"
+ Booleanos "Bool ∫"
+ 
+ */
+var str = "Hola mundo"
+
+str = "Daniel"
+
+//No puede cambiar
+let constante = "Motivacional"
+
